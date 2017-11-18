e37a09edb26029351f0d4533701892fb684a2659
Rick Gallegos <duulketariakan@yahoo.com>
2017-11-18 12:37:17 -0800

    Server static assets

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
