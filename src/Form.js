80c4d8cbe448761cb17dc12d7ed945d90f4d09f3
Heather Park <hthr.prk@gmail.com>
2017-11-18 14:30:07 -0800

    Style recipient form

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
+import { FormControl, FormGroup, Col, Row, Grid } from 'react-bootstrap';
 
 export default class Form extends Component {
   constructor() {
@@ -17,55 +17,76 @@ export default class Form extends Component {
 
   render() {
     return (
-      <div className="container">
-        <form>
-          <FormGroup controlId="formBasicText">
-            <ControlLabel>Name</ControlLabel>
-            <FormControl
-              type="text"
-              value={this.state.fromName}
-              placeholder="Name"
-              onChange={(event) => { this.setState({ fromName: event.target.value }) }}
-            />
-            <ControlLabel>Address Line 1</ControlLabel>
-            <FormControl
-              type="text"
-              value={this.state.fromAddressLine1}
-              placeholder="Address Line 1"
-              onChange={(event) => { this.setState({ fromAddressLine1: event.target.value }) }}
-            />
-            <ControlLabel>Address Line 2</ControlLabel>
-            <FormControl
-              type="text"
-              value={this.state.fromAddressLine2}
-              placeholder="Address Line 2"
-              onChange={(event) => { this.setState({ fromAddressLine2: event.target.value }) }}
-            />
-            <ControlLabel>City</ControlLabel>
-            <FormControl
-              type="text"
-              value={this.state.fromAddressCity}
-              placeholder="City"
-              onChange={(event) => { this.setState({ fromAddressCity: event.target.value }) }}
-            />
-            <ControlLabel>State</ControlLabel>
-            <FormControl
-              type="text"
-              value={this.state.fromAddressState}
-              placeholder="State"
-              onChange={(event) => { this.setState({ fromAddressState: event.target.value }) }}
-            />
-            <ControlLabel>Zip Code</ControlLabel>
-            <FormControl
-              type="text"
-              value={this.state.fromAddressZip}
-              placeholder="Zip Code"
-              onChange={(event) => { this.setState({ fromAddressZip: event.target.value }) }}
-            />
-          <FormControl.Feedback />
-        </FormGroup>
+      <form>
+        <Grid>
+          <Row>
+            <Col xs={12} md={6}>
+              <FormGroup>
+                <FormControl
+                  type="text"
+                  value={this.state.fromName}
+                  placeholder="Name"
+                  onChange={(event) => { this.setState({ fromName: event.target.value }) }}
+                />
+              </FormGroup>
+              <FormGroup>
+                <FormControl
+                  type="text"
+                  value={this.state.fromAddressLine1}
+                  placeholder="Address Line 1"
+                  onChange={(event) => { this.setState({ fromAddressLine1: event.target.value }) }}
+                />
+              </FormGroup>
+              <FormGroup>
+
+                <FormControl
+                  type="text"
+                  value={this.state.fromAddressLine2}
+                  placeholder="Address Line 2"
+                  onChange={(event) => { this.setState({ fromAddressLine2: event.target.value }) }}
+                />
+              </FormGroup>
+
+              <FormGroup>
+
+                <FormControl
+                  type="text"
+                  value={this.state.fromAddressCity}
+                  placeholder="City"
+                  onChange={(event) => { this.setState({ fromAddressCity: event.target.value }) }}
+                />
+              </FormGroup>
+
+              <Row>
+                <Col md={6}>
+                  <FormGroup>
+
+                    <FormControl
+                      type="text"
+                      value={this.state.fromAddressState}
+                      placeholder="State"
+                      onChange={(event) => { this.setState({ fromAddressState: event.target.value }) }}
+                    />
+                  </FormGroup>
+
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+
+                    <FormControl
+                      type="text"
+                      value={this.state.fromAddressZip}
+                      placeholder="Zip Code"
+                      onChange={(event) => { this.setState({ fromAddressZip: event.target.value }) }}
+                    />
+                  </FormGroup>
+
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
       </form>
-    </div>
     )
   }
 }
