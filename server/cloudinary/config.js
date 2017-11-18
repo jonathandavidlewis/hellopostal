8e9f047712b2d0975f511da6d38f592d2411e920
Rick Gallegos <duulketariakan@yahoo.com>
2017-11-18 13:43:33 -0800

    Add .env.example file

---
+++ server/cloudinary/config.js
@@ -0,0 +1,5 @@
+import cloudinary from 'cloudinary';
+
+const CLOUDINARY_KEY = process.env.CLOUDINARY_API_KEY;
+const CLOUDINARY_SEC = process.env.CLOUDINARY_API_SEC;
+const CLOUDINARY_NAME = process.env.CLOUDINARY_API_NAME;
