58e5a2f8893f6cf6726fb54a8c9e545d31c32217
Rick Gallegos <duulketariakan@yahoo.com>
2017-11-18 12:09:46 -0800

    Add initial home route

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-// const app = require('server/request-handler.js');
+const app = require('server/request-handler.js');
 
-// It Begins
+const PORT = process.env.PORT || 6060;
+const HOST = process.env.HOST || 'Local host';
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening at http at http://${HOST}:${PORT}`);
+});
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,4 +1,14 @@
-// const app = require('express');
-// const bodyParser = require('body-parser');
-//
-// const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log('Check home route');
+  res.end('home page');
+});
+
+module.exports = app;
