4fad2123e300e663a7e454a05916840fd338876f
Rick Gallegos <duulketariakan@yahoo.com>
2017-11-18 14:40:07 -0800

    Fix grammar

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 6060;
 const HOST = process.env.HOST || 'localhost';
 
 const server = app.listen(PORT, () => {
-  console.log(`Listening at http at http://${HOST}:${PORT}`);
+  console.log(`Listening at http://${HOST}:${PORT}`);
 });
