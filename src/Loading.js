d2c72a9a01cc13b08177672493e8d019c48b548e
Heather Park <hthr.prk@gmail.com>
2017-11-19 15:32:52 -0800

    Make Loading component

---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+
+const Loading = () => (
+  <Row className="app-chrome vertical-align-container">
+    <p className="loading text-center vertical-align-item">submitting your information...</p>
+  </Row>
+);
+
+export default Loading;
