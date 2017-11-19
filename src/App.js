1fc2c37bd7023808b1be57eab08fe208f8f80d8d
Heather Park <hthr.prk@gmail.com>
2017-11-19 11:10:44 -0800

    Add Confirmation component and React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
-import AppChrome from './AppChrome.js';
-
+import Form from './Form.js';
+import Confirmation from './Confirmation.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppChrome />
-      </div>
+      <Router>
+        <div className="App">
+          <div className="app-chrome">
+            <div className="logo-header">
+              <h1 className="app-logo">hellopostal</h1>
+            </div>
+            <Route exact path="/" component={Form} />
+            <Route exact path="/confirmation" component={Confirmation} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Confirmation.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Confirmation = () => (
+  <div>
+    Success
+  </div>
+)
+
+export default Confirmation
