5e1e18cc9319767cda20b21609382a4d518e8260
Jonathan Lewis <jonathandavidlewis@gmail.com>
2017-11-19 16:34:45 -0800

    fix cloudinary

--- server/routes/cloudinary.js
+++ server/routes/cloudinary.js
@@ -7,13 +7,14 @@ exports.uploadCloud = (req, res, next) => {
     const photoPath = req.files[0].path;
     const photoName = req.files[0].originalname;
 
-  uploadPhoto(photoPath, photoName)
-    .then((response) => {
-      req.body.imageUrl = response.secure_url;
-      req.body.image_public_id = `${response.public_id}.${response.format}`;
-      next();
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    uploadPhoto(photoPath, photoName)
+      .then((response) => {
+        req.body.imageUrl = response.secure_url;
+        req.body.image_public_id = `${response.public_id}.${response.format}`;
+        next();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 };
